40757e075030bd5b4abafa2f2abd40688260aaa0
sakshi srivastava <sakshisrivastava413@gmail.com>
2018-09-30 06:08:52 +0530

    made important declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchService } from './search.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
